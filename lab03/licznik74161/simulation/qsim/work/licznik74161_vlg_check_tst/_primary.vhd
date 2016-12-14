9c5b06a6a45e6ed834a35d06cbfd11f90eab3028
Adrian Wawrzak <adiq94@gmail.com>
2016-12-14 22:19:12 +0100

    seems to work...

---
+++ lab03/licznik74161/simulation/qsim/work/licznik74161_vlg_check_tst/_primary.vhd
@@ -0,0 +1,14 @@
+library verilog;
+use verilog.vl_types.all;
+entity licznik74161_vlg_check_tst is
+    port(
+        pin_name3       : in     vl_logic;
+        pin_name4       : in     vl_logic;
+        pin_name5       : in     vl_logic;
+        pin_name6       : in     vl_logic;
+        pin_name7       : in     vl_logic;
+        pin_name8       : in     vl_logic;
+        pin_name9       : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end licznik74161_vlg_check_tst;
---
+++ lab03/licznik74161/simulation/qsim/work/licznik74161_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity licznik74161_vlg_vec_tst is
+end licznik74161_vlg_vec_tst;
